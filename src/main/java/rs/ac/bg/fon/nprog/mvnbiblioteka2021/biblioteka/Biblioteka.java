f8ac1d8bdbe176a1720648baf9a5c6429de7026f
teodora8513 <jevtic.teodora@gmail.com>
2021-03-23 22:23:05 +0100

    Dodate klase iz drugog projekta

---
+++ src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/Biblioteka.java
@@ -0,0 +1,61 @@
+package rs.ac.bg.fon.nprog.mvnbiblioteka2021.biblioteka;
+
+
+
+import java.util.LinkedList;
+
+
+import rs.ac.bg.fon.nprog.mvnbiblioteka2021.biblioteka.interfejs.BibliotekaInterfejs;
+
+/**
+ * Ova klasa implementira interfejs BibliotekaInterface i predstavlja implementaciju preko Liste.
+ * 
+ * @author Bojan Tomic
+ *
+ */
+public class Biblioteka implements BibliotekaInterfejs {
+
+	/**
+	 * Lista sa svim knjigama u biblioteci.
+	 */
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga vec postoji");
+		
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null || !knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne sme biti null i mora postojati u biblioteci");
+		
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if (autor == null && isbn == null && naslov == null && izdavac == null)
+			throw new RuntimeException("Morate uneti bar jedan kriterijum pretrage");
+		
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for(Knjiga knjiga: knjige)
+			if (knjiga.getNaslov().contains(naslov))
+				rezultat.add(knjiga);
+		
+		return rezultat;
+	}
+
+}
